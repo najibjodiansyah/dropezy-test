e445579dd8f948446703f7f33b9ab178bc9a249e
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-02-08 18:30:40 +0700

    add testing for add and remove

--- main.go
+++ main.go
@@ -16,12 +16,11 @@ type store struct{
 	name string
 }
 
-func (w *warehouse)AddProduct(p product) {
+func (w *warehouse)AddProduct(s store, p product) {
 	 w.item = append(w.item, p)
-	 fmt.Println("add")
 } 
 
-func (w *warehouse)RemoveProduct(p product) {
+func (w *warehouse)RemoveProduct(s store, p product) {
 	result := make([]product, len(w.item)-1)
 	for _, v := range w.item {
 		if v.name != p.name {
@@ -35,25 +34,36 @@ func main() {
 	
 	fmt.Println("test dropezy")
 	var gudang warehouse
-	var produk1 product = product{
+	var (
+	produk1 product = product{
 		name: "Ciki",
 		stock: 1,
 		price: 1,
 	}
-	var produk2 product = product{
+	produk2 product = product{
 		name: "Soda",
 		stock: 2,
 		price: 1,
 	}
-	var produk3 product = product{
+	produk3 product = product{
 		name: "Minyak",
 		stock: 3,
 		price: 1,
 	}
-	gudang.AddProduct(produk1)
+	store1 store = store{
+		name: "jakarta barat",
+	}
+	store2 store = store{
+		name: "jakarta timur",
+	}
+	store3 store = store{
+		name: "jakarta selatan",
+	}
+	)
+	gudang.AddProduct(store1, produk1)
 	fmt.Println("yang ada di gudang",gudang.item)
-	gudang.AddProduct(produk2)
-	gudang.AddProduct(produk3)
+	gudang.AddProduct(store2, produk2)
+	gudang.AddProduct(store3, produk3)
 	fmt.Println("yang ada di gudang",gudang.item)
 
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,24 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 var (
+	store1 store = store{
+		name: "jakarta barat",
+	}
+	store2 store = store{
+		name: "jakarta timur",
+	}
 	product1 product = product{
 		name: "shampo",
+		stock: 1,
+		price: 10,
+	}
+	product2 product = product{
+		name: "sabun",
 		stock: 1,
 		price: 10,
 	}
@@ -13,8 +27,30 @@ var (
 
 func TestAddProduct(t *testing.T){
 	expectation := product1
-	gudang.AddProduct(product1)
-	equal := gudang.item[0]
+	var result product
+	gudang.AddProduct(store1, product1)
+	gudang.AddProduct(store2, product2)
+	for _, v := range gudang.item {
+		if v.name == product1.name{
+			result = product1
+		}
+	}
+	equal := result
+	fmt.Println(expectation,equal,gudang)
+	if equal != expectation {
+		t.Errorf("Expected %v but got %v", expectation, equal)
+	}
+}
+
+func TestRemoveProduct(t *testing.T){
+	gudang.RemoveProduct(store1, product1)
+	expectation := true
+	equal := false
+	for _, v := range gudang.item {
+		if v.name != product1.name{
+			equal = true
+		}
+	}
 	if equal != expectation {
 		t.Errorf("Expected %v but got %v", expectation, equal)
 	}
